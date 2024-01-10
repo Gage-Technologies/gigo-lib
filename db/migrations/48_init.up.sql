932dcf0d219298fe6a73f045f670dbd6c6f8cf83
Daniel Ulrich <tmwap76@gmail.com>
2024-01-10 16:30:58 -0600

    Fix workspace_table_id in workspace_pool table to accept null

--- db/migrations/48_init.up.sql
+++ db/migrations/48_init.up.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS workspace_pool (
     `volume_size` bigint not null,
     `secret`  binary(16) not null,
     `agent_id` bigint not null,
-    `workspace_table_id` bigint not null
+    `workspace_table_id` bigint
 );
 
 create table if not exists bytes(
