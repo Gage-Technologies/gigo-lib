2fa9cc815f287b49103d95e4d524978994a38336
Sam Ulrich <samson98ulrich@gmail.com>
2024-03-27 11:51:27 -0500

    Add expiration to workspace pool

--- db/migrations/61_init.up.sql
+++ db/migrations/61_init.up.sql
@@ -2,3 +2,6 @@
 
 -- add creation_start_timestamp to workspace_pool
 ALTER TABLE workspace_pool ADD COLUMN create_start_timestamp TIMESTAMP;
+
+-- add expiration to workspace_pool
+ALTER TABLE workspace_pool ADD COLUMN expiration TIMESTAMP;
